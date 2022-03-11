5921b878828d6b70fa77d59ee953e765578ce5fa
Jagannath <jneupane07@gmail.com>
2022-03-11 19:48:07 +1100

    exception handeling added to the code

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -1,4 +1,5 @@
-import random
+import random, re
+# import sys
 
 itemsList =['rock','paper','scissor']
 
@@ -14,32 +15,39 @@ while(numberOfGame<5):
     # scissor beats paper
     # paper beats rock
 
-    userChoice = input("Enter your choice, [Type in lowercase or small letters]  ")
-    if userChoice == computerChoice:
-        print("It's a draw. Play again")
-        numberOfdraw=numberOfdraw+1
+    try:
+        userChoice = input("Enter your choice, [Type in lowercase or small letters] ")
+        choices = ["rock","paper","scissor"]
+        if userChoice.lower() not in choices:
+            print("Input Error! Please enter [rock, paper or scissor] to play")
+            # sys.exit() # this will terminate the program
+        if userChoice == computerChoice:
+            print("It's a draw. Play again")
+            numberOfdraw=numberOfdraw+1
 
-    if userChoice =='rock' and computerChoice=='scissor':
-        print("You Won")
-        userScore =userScore+1
-    elif userChoice=='rock' and computerChoice=='paper':
-        print("Computer Won")
-        computerScore=computerScore+1
-    if userChoice == 'scissor' and computerChoice =='paper':
-        print("You Won")
-        userScore =userScore+1
-    elif userChoice=='scissor' and computerChoice=='rock':
-        print("Computer Won")
-        computerScore=computerScore+1
+        if userChoice =='rock' and computerChoice=='scissor':
+            print("You Won")
+            userScore =userScore+1
+        elif userChoice=='rock' and computerChoice=='paper':
+            print("Computer Won")
+            computerScore=computerScore+1
+        if userChoice == 'scissor' and computerChoice =='paper':
+            print("You Won")
+            userScore =userScore+1
+        elif userChoice=='scissor' and computerChoice=='rock':
+            print("Computer Won")
+            computerScore=computerScore+1
 
-    if userChoice =='paper' and computerChoice =='rock':
-        print('you Won')
-        userScore =userScore+1
-    elif userChoice =="paper" and computerChoice=="scissor":
-        print("Computer Won")
-        computerScore=computerScore+1
+        if userChoice =='paper' and computerChoice =='rock':
+            print('you Won')
+            userScore =userScore+1
+        elif userChoice =="paper" and computerChoice=="scissor":
+            print("Computer Won")
+            computerScore=computerScore+1
 
-    numberOfGame=numberOfGame+1
+        numberOfGame=numberOfGame+1
+    except KeyboardInterrupt:
+        print(" KeyboardInterrupt exception is caught, Please enter correctly.")
 print('\n')
 print("***GAME SUMMARY***")
 print("You played",numberOfGame,"times")
